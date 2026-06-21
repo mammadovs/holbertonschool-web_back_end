8d24b1769c105ec8192bb122c9a7ab166e47f8b8
mammadovs <12747@holbertonstudents.com>
2026-06-21 11:43:28 +0000

    8. List all documents in Python

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module that contains the function list_all
+"""
+
+
+def list_all(mongo_collection):
+"""
+    Lists all documents in a collection.
+    Returns an empty list if no documents are found.
+    """
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
+
