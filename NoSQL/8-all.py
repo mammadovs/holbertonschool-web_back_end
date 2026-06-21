7fac703d11f63413ecb51c9ab068d659bac6768c
mammadovs <12747@holbertonstudents.com>
2026-06-21 11:44:17 +0000

    8. List all documents in Python

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -5,7 +5,7 @@ Module that contains the function list_all
 
 
 def list_all(mongo_collection):
-"""
+    """
     Lists all documents in a collection.
     Returns an empty list if no documents are found.
     """
