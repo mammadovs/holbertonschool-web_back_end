1954aa1ae8758323e38f013564c8e38b90d94fb0
mammadovs <12747@holbertonstudents.com>
2026-06-25 08:56:07 +0000

    0. Const or let?

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-var': 'error',
+    'prefer-const': 'error',
+  },
+};
