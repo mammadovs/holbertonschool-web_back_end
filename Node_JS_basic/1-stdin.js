59f414cb3e38c79d9efda5b2680fe7e41a3fbea3
mammadovs <12747@holbertonstudents.com>
2026-07-25 09:13:15 +0000

    feat(nodejs-basics): 1. Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+/**
+ * Interactive program that reads input from STDIN and greets the user
+ */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
