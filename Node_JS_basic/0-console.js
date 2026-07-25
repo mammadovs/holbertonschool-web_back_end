42a6e203a1175e0a378fc118610b032ad5a8c8a2
mammadovs <12747@holbertonstudents.com>
2026-07-25 08:46:34 +0000

    feat(nodejs-basics): 0. Executing basic javascript with Node JS

---
+++ Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display a message on the standard output
+ * @param {string} text - The message to print
+ */
+const displayMessage = (text) => {
+  console.log(text);
+};
+
+module.exports = displayMessage;
+
